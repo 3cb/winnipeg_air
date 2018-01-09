8e5864ede76357a8c1ed840c52fa8c1f6eced0dd
Marc CB <marc.cbrooks@gmail.com>
2018-01-09 14:43:30 -0500

    add basic routing

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 
 	"github.com/3cb/ssc"
 
@@ -31,4 +34,13 @@ func main() {
 	}
 
 	go startPolling(db, pool)
+
+	// routes
+	r := mux.NewRouter()
+
+	r.Handle("/", http.FileServer(http.Dir("./static/")))
+	r.PathPrefix("/dist").Handler(http.FileServer(http.Dir("./static/")))
+
+	// start server
+	log.Fatal(http.ListenAndServe(":4040", r))
 }
